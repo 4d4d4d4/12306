ad64f44e8d21191e484b090711941e930ea8c785
Qing Yuan Cheng <103577532+4d4d4d4@users.noreply.github.com>
2024-07-12 14:47:04 +0800

    初始化会员模块

---
+++ train-member/src/main/java/com/train/member/MemberApplication.java
@@ -0,0 +1,17 @@
+package com.train.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Classname MemberApplication
+ * @Description 会员模块~
+ * @Date 2024/7/12 下午2:38
+ * @Created by 憧憬
+ */
+@SpringBootApplication
+public class MemberApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class, args);
+    }
+}
