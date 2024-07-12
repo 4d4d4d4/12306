bc2a4f33f692d8db960d8df0c8d9f5190d88b476
Qing Yuan Cheng <103577532+4d4d4d4@users.noreply.github.com>
2024-07-12 15:12:58 +0800

    增加启动日志

--- train-member/src/main/java/com/train/member/MemberApplication.java
+++ train-member/src/main/java/com/train/member/MemberApplication.java
@@ -1,7 +1,12 @@
 package com.train.member;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
 
 /**
  * @Classname MemberApplication
@@ -11,7 +16,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class MemberApplication {
+    private static final Logger logger  = LoggerFactory.getLogger(MemberApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(MemberApplication.class, args);
+        ConfigurableEnvironment environment = run.getEnvironment();
+        String startPort = environment.getProperty("server.port");
+        logger.info("会员服务启动：{}", ("http://localhost:" + startPort));
+
     }
 }
